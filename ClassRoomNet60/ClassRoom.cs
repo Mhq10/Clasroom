fd8cd6b06ce88bf8ad985d0a06092899c5ba6012
Martin Alejandro Hernandez Quintana <mart01f8@edu.zealand.dk>
2026-09-07 10:59:22 +0200

    task 1-2

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,16 @@
+﻿
+    public class ClassRoom
+    {
+        public string ClassName { get; set; }
+        public List<Student> Students { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+    public ClassRoom(string className, List<Student> students, DateTime semesterStart)
+    {
+        ClassName = className;
+        Students = students;
+        SemesterStart = semesterStart;
+    }
+
+}
+
---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,17 @@
+﻿
+
+    public class Student
+
+{
+    public string Name { get; set; }
+    public int BirthMonth { get; set; }
+    public int Birthday { get; set; }
+
+    public Student(string name, int birthMonth, int birthday)
+    {
+        Name = name;
+        BirthMonth = birthMonth;
+        Birthday = birthday;
+    }
+}
+
