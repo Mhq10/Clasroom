c9f8fcee51d458ba0bf2b62c2415d625f21b4f05
Martin Alejandro Hernandez Quintana <mart01f8@edu.zealand.dk>
2026-09-07 12:21:35 +0200

    Task 5

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,12 +1,19 @@
 ﻿
 
-ClassRoom classRoom = new ClassRoom("Programming 3", new List<Student>, new DateTime(2026, 08, 26));
-
-List<Student> students = new List<Student>
+    List<Student> students = new List<Student>
 {
     new Student("Martin", 10, 15),
     new Student("Christoffer", 8, 22),
     new Student("Nicklas", 12, 3),
     new Student("Simon", 7, 3)
-
 };
+
+ClassRoom classRoom = new ClassRoom("Programming 3", students, new DateTime(2026, 08, 26));
+
+Console.WriteLine($"Class Name: {classRoom.ClassName}");
+Console.WriteLine($"Semester Start: {classRoom.SemesterStart}");
+
+foreach (var student in classRoom.Students)
+{
+    Console.WriteLine($"{student.Name} - Birthday: {student.Birthday}/{student.BirthMonth}");
+}
--- ClassRoomNet60/Student.cs
+++ ClassRoomNet60/Student.cs
@@ -13,5 +13,34 @@
         BirthMonth = birthMonth;
         Birthday = birthday;
     }
+
+    public string Season ()
+    {
+        if (BirthMonth == 12 || BirthMonth == 1 || BirthMonth == 2)
+        {
+            return "Winter";
+        }
+        else if (BirthMonth == 3 || BirthMonth == 4 || BirthMonth == 5)
+        {
+            return "Spring";
+        }
+        else if (BirthMonth == 6 || BirthMonth == 7 || BirthMonth == 8)
+        {
+            return "Summer";
+        }
+        else if (BirthMonth == 9 || BirthMonth == 10 || BirthMonth == 11)
+        {
+            return "Autumn";
+        }
+        else
+        {
+            return "Invalid month";
+        }
+
+    }
+
+
+
+
 }
 
