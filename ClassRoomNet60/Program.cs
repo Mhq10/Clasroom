60e5852285764949a09b38583ed6785a7dd5fe0e
Martin Alejandro Hernandez Quintana <mart01f8@edu.zealand.dk>
2026-09-07 11:07:36 +0200

    task 3

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+ClassRoom classRoom = new ClassRoom("Programming 3", new List<Student>, new DateTime(2026, 08, 26));
+
+List<Student> students = new List<Student>
+{
+    new Student("Martin", 10, 15),
+    new Student("Christoffer", 8, 22),
+    new Student("Nicklas", 12, 3),
+    new Student("Simon", 7, 3)
+
+};
